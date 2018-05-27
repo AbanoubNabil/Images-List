f11ad2acddff62c0d4f175281161303ece4f11e9
Abanoub Nabil <sayedabdo@Abanoub-Nabil.local>
2018-05-27 11:21:58 -0700

    Network layer Launch

---
+++ MojazTask/Helper/NetworkServiceLayer.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkServiceLayer.swift
+//  MojazTask
+//
+//  Created by Sayed Abdo on 5/27/18.
+//  Copyright © 2018 Bombo. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+class NetworkServiceLayer: NSObject {
+    
+    
+    func fetchRequest(completion: @escaping (JSON) -> Void , myurl : String) {
+        guard let url = URL(string: myurl) else {
+            completion(JSON.null)
+            return
+        }
+        //make a request
+        Alamofire.request(url,method: .get).validate().responseJSON { response in
+            //1 check result
+            guard response.result.isSuccess else {
+                print(" Error while fetching remote rooms: \(String(describing:response.result.error))")
+                completion(JSON.null)
+                return
+            }
+            //2 result value
+            let json = JSON(response.value!)
+            completion(json)
+        }
+    }
+}
---
+++ MojazTask/ImageListController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MojazTask
+//
+//  Created by Sayed Abdo on 5/27/18.
+//  Copyright © 2018 Bombo. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+class ImageListController : UIViewController {
+
+    var networkObj = NetworkServiceLayer()
+    lazy var handlerBlock: (JSON) -> Void = { items in
+        print("test url \(items)")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     networkObj.fetchRequest(completion: handlerBlock, myurl: "https://jsonplaceholder.typicode.com/photos")
+        
+    }
+
+}
+
